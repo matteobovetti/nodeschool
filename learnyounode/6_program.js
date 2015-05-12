0ff99e156567097669f2ae96ccfc263d0eb724fb
Matteo Bovetti <Matteo@Pc-Matteo.edp.local>
2015-05-12 11:48:28 +0200

    minor.

--- learnyounode/6_program.js
+++ learnyounode/6_program.js
@@ -1,6 +1,5 @@
 var mymodule = require('./6_module.js');
 
-
 mymodule(process.argv[2], process.argv[3], function(err, data) {
     if (err)
         return console.error('There is an error:', err);
